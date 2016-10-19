2bf9e8195548f577ba15a5e9570193eb24f76a18
Chuck Winston <cwinston@technotainment.com>
2016-10-19 01:22:29 -0500

    added new controlling object GoogleMaps which is used by the UI component but also handles signals and incoming messages from javascript

---
+++ GoogleMaps.cpp
@@ -0,0 +1,176 @@
+#include "GoogleMaps.h"
+#include <QDebug>
+
+googleMaps::GoogleMaps::GoogleMaps(QObject *parent):
+    QObject(parent)
+{
+    m_transportReady = false;
+}
+
+googleMaps::GoogleMaps::~GoogleMaps()
+{
+}
+
+QWebChannel* googleMaps::GoogleMaps::getChannel() const
+{
+    return m_channel;
+}
+
+void googleMaps::GoogleMaps::setChannel(QWebChannel* chnl)
+{
+    qDebug() << "Setting channel " ;
+    m_channel = chnl;
+    m_transportReady = true;
+    m_geoCoder = new googleMaps::Geocoder(this);
+    m_sphericalGeometryService = new googleMaps::SphericalGeometry(this);
+    m_elevationService = new googleMaps::ElevationService(this);
+   // m_maxZoomService = new googleMaps::MaxZoomService(this);
+    m_channel->registerObject(QStringLiteral("Geocoder"), m_geoCoder);
+    m_channel->registerObject(QStringLiteral("ElevationService"), m_elevationService);
+    m_channel->registerObject(QStringLiteral("SphericalGeometry"), m_sphericalGeometryService);
+ //   m_channel->registerObject(QStringLiteral("MaxZoom"), m_maxZoomService);
+    emit transportReady();
+}
+
+bool googleMaps::GoogleMaps::isConnected()
+{
+   return m_transportReady;
+}
+
+void googleMaps::GoogleMaps::computeArea(const QList<googleMaps::LatLng>& path)
+{
+    m_sphericalGeometryService->computeArea(path);
+}
+
+void googleMaps::GoogleMaps::computeDistanceBetween(const googleMaps::LatLng& from, const googleMaps::LatLng& to) {
+    throw "Not yet implemented";
+}
+
+void googleMaps::GoogleMaps::computeHeading(const googleMaps::LatLng& from, const googleMaps::LatLng& to) {
+    throw "Not yet implemented";
+}
+
+void googleMaps::GoogleMaps::computeLength(const QList<googleMaps::LatLng>& path) {
+    throw "Not yet implemented";
+}
+
+void googleMaps::GoogleMaps::computeOffset(const googleMaps::LatLng& from, const qreal& distance, const qreal& heading) {
+    throw "Not yet implemented";
+}
+
+void googleMaps::GoogleMaps::computeOffsetOrigin(const googleMaps::LatLng& to, const qreal& distance, const qreal& heading) {
+    throw "Not yet implemented";
+}
+
+void googleMaps::GoogleMaps::interpolate(googleMaps::LatLng& from, googleMaps::LatLng& to, qreal& fraction) {
+    throw "Not yet implemented";
+}
+
+void googleMaps::GoogleMaps::getMaxZoomAtLatLng(googleMaps::LatLng latLng) {
+    throw "Not yet implemented";
+}
+
+void googleMaps::GoogleMaps::geocode(QString& location)
+{
+    googleMaps::GeocoderRequest request = googleMaps::Geocoder::createGeocoderRequest(location);
+    connect(m_geoCoder, SIGNAL(geocodeResultReceived(QList<googleMaps::GeocoderResult>&,QString&)), this, SLOT(handleGeocoderResults(QList<googleMaps::GeocoderResult>&,QString&)));
+    m_geoCoder->geocode(request);
+}
+
+
+void googleMaps::GoogleMaps::geocode(googleMaps::LatLng& latLng) {
+    throw "Not yet implemented";
+}
+
+void googleMaps::GoogleMaps::getElevationAlongPath(googleMaps::PathElevationRequest request) {
+    throw "Not yet implemented";
+}
+
+void googleMaps::GoogleMaps::getElevationForLocations(googleMaps::LocationElevationRequest request) {
+    throw "Not yet implemented";
+}
+
+void googleMaps::GoogleMaps::messageReceived(const QJsonObject& message)
+{
+     QJsonDocument doc(message);
+
+    QString strJson(doc.toJson(QJsonDocument::Compact));
+    //QVariantMap messageMap = message.toVariantMap();
+    qDebug() << " Json message received " << strJson;
+}
+
+void googleMaps::GoogleMaps::updateGeocoderResults(QList<googleMaps::GeocoderResult>& results, QString& status)
+{
+    qDebug() << "[GoogleMaps] handleGeocoderResults " << results.size();
+    if (results.size() == 1)
+    {
+        emit geoCoderResponseReceived(results.at(0));
+    }
+    else if (results.size() > 1)
+    {
+        emit selectGeoLocationRequest(results);
+    }
+}
+
+void googleMaps::GoogleMaps::updateDistanceResults(qreal distance) {
+    throw "Not yet implemented";
+}
+
+void googleMaps::GoogleMaps::updatePositionResults(googleMaps::LatLng position) {
+    throw "Not yet implemented";
+}
+
+void googleMaps::GoogleMaps::updateMaxZoomResults(googleMaps::MaxZoomResult result, int status) {
+    throw "Not yet implemented";
+}
+
+void googleMaps::GoogleMaps::updateElevationResults(QList<googleMaps::ElevationResult> results, int status) {
+    throw "Not yet implemented";
+}
+
+void googleMaps::GoogleMaps::handleAreaComputed(qreal area) {
+    throw "Not yet implemented";
+}
+
+void googleMaps::GoogleMaps::handleHeadingComputed(qreal heding) {
+    throw "Not yet implemented";
+}
+
+void googleMaps::GoogleMaps::handleDistanceComputed(qreal distance) {
+    throw "Not yet implemented";
+}
+
+void googleMaps::GoogleMaps::handleGeoLocationReceived(googleMaps::GeocoderResult &result) {
+    qDebug() << "[GeoWorldBuilder]   GeoLocation Received ";
+//     disconnect(m_viewer, SIGNAL(geoCoderResponseReceived(googleMaps::GeocoderResult&)), this, SLOT(handleGeoLocationReceived(googleMaps::GeocoderResult&)));
+//     disconnect(m_viewer, SIGNAL(selectGeoLocationRequest(QList<googleMaps::GeocoderResult>&)), this, SLOT(handleSelectGeoLocationRequest(QList<googleMaps::GeocoderResult>&)));
+}
+
+void googleMaps::GoogleMaps::handleSelectGeoLocationRequest(QList<googleMaps::GeocoderResult>& results)
+{
+    qDebug() << "[GeoWorldBuilder] need2  SelectGeoLocationRequest ";
+//    disconnect(m_viewer, SIGNAL(geoCoderResponseReceived(googleMaps::GeocoderResult&)), this, SLOT(handleGeoLocationReceived(googleMaps::GeocoderResult&)));
+//    disconnect(m_viewer, SIGNAL(selectGeoLocationRequest(QList<googleMaps::GeocoderResult>&)), this, SLOT(handleSelectGeoLocationRequest(QList<googleMaps::GeocoderResult>&)));
+}
+
+void googleMaps::GoogleMaps::handleLengthComputed(qreal length) {
+    throw "Not yet implemented";
+}
+
+void googleMaps::GoogleMaps::handleOffsetComputed(googleMaps::LatLng offset) {
+    throw "Not yet implemented";
+}
+
+void googleMaps::GoogleMaps::handleOffsetOriginComputed(googleMaps::LatLng offsetOrigin) {
+    throw "Not yet implemented";
+}
+
+void googleMaps::GoogleMaps::handlePositionInterpolated(googleMaps::LatLng originOffset) {
+    throw "Not yet implemented";
+}
+
+void googleMaps::GoogleMaps::handleMaxZoomReceived(qreal zoomLevel) {
+    throw "Not yet implemented";
+}
+
+
---
+++ GoogleMaps.h
@@ -0,0 +1,145 @@
+/*
+ *
+ *  Needs to expose all the signals and slots from MapViewer
+ *
+ *
+ *
+ */
+
+#ifndef GOOGLEMAPS_H
+#define GOOGLEMAPS_H
+
+#include <QObject>
+#include <QWebChannelAbstractTransport>
+#include <QWebChannel>
+#include "GeocoderRequest.h"
+#include "GeocoderComponentRestrictions.h"
+#include "GeocoderAddressComponent.h"
+#include "GeocoderGeometry.h"
+#include "SphericalGeometry.h"
+#include "MaxZoomService.h"
+#include "Geocoder.h"
+#include "SphericalGeometry.h"
+#include "MaxZoomService.h"
+#include "ElevationService.h"
+#include "LatLng.h"
+#include "GeocoderResult.h"
+#include "ElevationResult.h"
+
+namespace googleMaps
+{
+    class GoogleMaps;
+}
+
+
+namespace googleMaps
+{
+    class GoogleMaps : public QObject
+    {
+        Q_OBJECT
+        Q_PROPERTY(QWebChannel* channel READ getChannel WRITE setChannel NOTIFY transportReady)
+        Q_PROPERTY(bool connectionMade READ isConnected NOTIFY transportReady)
+
+    public:
+        explicit GoogleMaps(QObject *parent = 0);
+        ~GoogleMaps();
+
+        QWebChannel* getChannel() const;
+        void setChannel(QWebChannel *chnl);
+        bool isConnected();
+
+    protected:
+        googleMaps::Geocoder* m_geoCoder;
+        googleMaps::SphericalGeometry* m_sphericalGeometryService;
+        googleMaps::MaxZoomService* m_maxZoomService;
+        googleMaps::ElevationService* m_elevationService;
+        QWebChannel* m_channel;
+        bool m_transportReady;
+
+    signals:
+            void distanceResultsReceived(qreal distance);
+            void positionResultsReceived(googleMaps::LatLng position);
+            void geoCoderResponseReceived(googleMaps::GeocoderResult results);
+            void maxZoomReceived(qreal zoomLevel);
+            void elevationResultsReceived(QList<googleMaps::ElevationResult> results);
+            void sendMessage(const QJsonObject &message);
+            void geoLocationReceived(googleMaps::GeocoderResult& result);
+            void selectGeoLocationRequest(QList<googleMaps::GeocoderResult>& results);
+            void transportReady();
+
+    public slots:
+            /// <summary>
+            ///
+            /// Return Value:  number
+            /// Return unit: meters
+            /// Returns the area of a closed path. The computed area uses the same units as the radius. The radius defaults to the Earth's radius in meters, in which case the area is in square meters.
+            /// </summary>
+            void computeArea(const QList<googleMaps::LatLng>& path);
+
+            /// <summary>
+            ///
+            /// Return Value:  number
+            /// Returns the distance, in meters, between two LatLngs. You can optionally specify a custom radius. The radius defaults to the radius of the Earth.
+            /// </summary>
+            void computeDistanceBetween(const googleMaps::LatLng& from, const googleMaps::LatLng& to);
+
+            /// <summary>
+            ///
+            /// Return Value:  number
+            /// Returns the heading from one LatLng to another LatLng. Headings are expressed in degrees clockwise from North within the range [-180,180).
+            /// </summary>
+            void computeHeading(const googleMaps::LatLng& from, const googleMaps::LatLng& to);
+
+            /// <summary>
+            ///
+            /// Return Value:  number
+            /// Returns the length of the given path.
+            /// </summary>
+            void computeLength(const QList<googleMaps::LatLng>& path);
+
+            /// <summary>
+            ///
+            /// Return Value:  LatLng
+            /// Returns the LatLng resulting from moving a distance from an origin in the specified heading (expressed in degrees clockwise from north).
+            /// </summary>
+            void computeOffset(const googleMaps::LatLng& from, const qreal& distance, const qreal& heading);
+
+            /// <summary>
+            /// Returns the location of origin when provided with a LatLng destination, meters travelled and original heading. Headings are expressed in degrees clockwise from North. This function returns null when no solution is available.
+            /// </summary>
+            void computeOffsetOrigin(const googleMaps::LatLng& to, const qreal& distance, const qreal& heading);
+
+            /// <summary>
+            /// Returns the LatLng which lies the given fraction of the way between the origin LatLng and the destination LatLng.
+            /// </summary>
+            void interpolate(googleMaps::LatLng& from, googleMaps::LatLng& to, qreal& fraction);
+            void getMaxZoomAtLatLng(googleMaps::LatLng latLng);
+            void geocode(QString& location);
+            void geocode(googleMaps::LatLng& latLng);
+            void getElevationAlongPath(googleMaps::PathElevationRequest request);
+            void getElevationForLocations(googleMaps::LocationElevationRequest request);
+            void messageReceived(const QJsonObject& message);
+            void updateGeocoderResults(QList<googleMaps::GeocoderResult> &results, QString &status);
+            void updateDistanceResults(qreal distance);
+            void updatePositionResults(googleMaps::LatLng position);
+            void updateMaxZoomResults(googleMaps::MaxZoomResult result, int status);
+            void updateElevationResults(QList<googleMaps::ElevationResult> results, int status);
+
+
+    private slots:
+        //void handleTransportReady();
+        void handleGeoLocationReceived(googleMaps::GeocoderResult& result);
+        void handleSelectGeoLocationRequest(QList<googleMaps::GeocoderResult>& results);
+        void handleAreaComputed(qreal area);
+        void handleHeadingComputed(qreal heding);
+        void handleDistanceComputed(qreal distance);
+        void handleLengthComputed(qreal length);
+        void handleOffsetComputed(googleMaps::LatLng offset);
+        void handleOffsetOriginComputed(googleMaps::LatLng offsetOrigin);
+        void handlePositionInterpolated(googleMaps::LatLng originOffset);
+        void handleMaxZoomReceived(qreal zoomLevel);
+
+
+    };
+}
+#endif // GOOGLEMAPS_H
