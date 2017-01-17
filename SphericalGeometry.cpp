dc3c0e1297f42b9b964cd93a6c4903da133d4aff
Chuck Winston <cwinston@technotainment.com>
2017-01-17 02:33:59 -0600

    #1547, #1539, #1538, #1505 time:1.5h state:Done commment:"Made Spherical Geometry QObject compliant. Turned all public methods to return void because the functions are asynchronous. Added new signals to send calls to Javascript"

--- SphericalGeometry.cpp
+++ SphericalGeometry.cpp
@@ -1,57 +1,88 @@
 #include <exception>
 using namespace std;
 
+#include <QDebug>
 #include "SphericalGeometry.h"
 #include "LatLng.h"
+#include "qtutils.h"
 
-googleMaps::SphericalGeometry::SphericalGeometry(QObject *parent) {
+googleMaps::SphericalGeometry::SphericalGeometry(QObject *parent)
+{
+    setParent(parent);
+     qDebug() << "[SphericalGeometry]  ";
+    m_distanceResult = -1;
+    m_positionResult = LatLng(-1,-1);
 }
 
-googleMaps::SphericalGeometry::~SphericalGeometry() {
 
+void googleMaps::SphericalGeometry::computeArea(QList<googleMaps::LatLng> path)
+{
+    qDebug() << "[SphericalGeometry]   computeAreaRequest: ";
+    //emit computeAreaRequest(convert2VariantList(path));
 }
 
-qreal googleMaps::SphericalGeometry::computeArea(QList<googleMaps::LatLng> path) {
-	throw "Not yet implemented";
+void googleMaps::SphericalGeometry::computeDistanceBetween(const LatLng from, const LatLng to)
+{
+    qDebug() << "[SphericalGeometry]   computeDistanceBetweenRequest: ";
+    emit computeDistanceBetweenRequest(from.lat(), from.lng(), to.lat(), to.lng());
 }
 
-qreal googleMaps::SphericalGeometry::computeDistanceBetween(googleMaps::LatLng from, googleMaps::LatLng to) {
-	throw "Not yet implemented";
+
+
+
+
+
+
+
+void googleMaps::SphericalGeometry::computeLength(const QList<googleMaps::LatLng> path)
+{
+    qDebug() << "[SphericalGeometry]   computeLengthRequest: ";
+   // emit computeLengthRequest(convert2VariantList(path));
 }
 
-qreal googleMaps::SphericalGeometry::computeHeading(googleMaps::LatLng from, googleMaps::LatLng to) {
-	throw "Not yet implemented";
+void googleMaps::SphericalGeometry::computeOffset(const googleMaps::LatLng from, const qreal distance, const qreal heading)
+{
+    qDebug() << "[SphericalGeometry]   computeOffsetRequest: ";
+    emit computeOffsetRequest(from.lat(), from.lng(), distance, heading);
 }
 
-qreal googleMaps::SphericalGeometry::computeLength(QList<googleMaps::LatLng> path) {
-	throw "Not yet implemented";
+void googleMaps::SphericalGeometry::computeOffsetOrigin(const googleMaps::LatLng to, const qreal distance, const qreal heading)
+{
+    qDebug() << "[SphericalGeometry]   computeOffsetOriginRequest: ";
+    emit computeOffsetOriginRequest(to.lat(), to.lng(), distance, heading);
 }
 
-googleMaps::LatLng googleMaps::SphericalGeometry::computeOffset(googleMaps::LatLng from, qreal distance, qreal heading) {
-	throw "Not yet implemented";
+void googleMaps::SphericalGeometry::interpolate(const LatLng from, const LatLng to, const qreal fraction)
+{
+    qDebug() << "[SphericalGeometry]   interpolateRequest: ";
+    emit interpolateRequest(from.lat(), from.lng(), to.lat(), to.lng(), fraction);
 }
 
-googleMaps::LatLng googleMaps::SphericalGeometry::computeOffsetOrigin(googleMaps::LatLng to, qreal distance, qreal heading) {
-	throw "Not yet implemented";
+void googleMaps::SphericalGeometry::computeHeading(const googleMaps::LatLng from, const googleMaps::LatLng to)
+{
+    qDebug() << "[SphericalGeometry]   computeHeadingRequest: ";
+    emit computeHeadingRequest(from.lat(), from.lng(), to.lat(), to.lng());
 }
 
-googleMaps::LatLng googleMaps::SphericalGeometry::interpolate(googleMaps::LatLng from, googleMaps::LatLng to, qreal fraction) {
-	throw "Not yet implemented";
-}
 
-void googleMaps::SphericalGeometry::setDistanceResult(const qreal aDistanceResult) {
+void googleMaps::SphericalGeometry::setDistanceResult(const qreal aDistanceResult)
+{
     this->m_distanceResult = aDistanceResult;
+    emit distanceResultReceived();
 }
 
-qreal googleMaps::SphericalGeometry::getDistanceResult() const {
+qreal googleMaps::SphericalGeometry::getDistanceResult() const
+{
     return this->m_distanceResult;
 }
 
-void googleMaps::SphericalGeometry::setPositionResult(const googleMaps::LatLng aPositionResult) {
+void googleMaps::SphericalGeometry::setPositionResult(const googleMaps::LatLng aPositionResult)
+{
     this->m_positionResult = aPositionResult;
 }
 
-googleMaps::LatLng googleMaps::SphericalGeometry::getPositionResult() const {
+googleMaps::LatLng googleMaps::SphericalGeometry::getPositionResults() const
+{
     return this->m_positionResult;
 }
 
--- SphericalGeometry.h
+++ SphericalGeometry.h
@@ -19,13 +19,15 @@ namespace googleMaps
     class SphericalGeometry : public QObject
 	{
         Q_OBJECT
+        Q_PROPERTY(googleMaps::LatLng positionResult READ getPositionResults NOTIFY positionResultReceived)
+        Q_PROPERTY(qreal distanceResult READ getDistanceResult NOTIFY distanceResultReceived)
         protected:
             qreal m_distanceResult;
             googleMaps::LatLng m_positionResult;
 
         public:
-            SphericalGeometry(QObject* parent = 0);
-            ~SphericalGeometry();
+            explicit SphericalGeometry(QObject* parent = 0);
+            SphericalGeometry(const googleMaps::SphericalGeometry &rhs, QObject* parent = 0);
 
             /// <summary>
             ///
@@ -33,51 +35,58 @@ namespace googleMaps
             /// Return unit: meters
             /// Returns the area of a closed path. The computed area uses the same units as the radius. The radius defaults to the Earth's radius in meters, in which case the area is in square meters.
             /// </summary>
-            qreal computeArea(QList<googleMaps::LatLng> path);
+            void computeArea(QList<googleMaps::LatLng> path);
 
             /// <summary>
             ///
             /// Return Value:  number
             /// Returns the distance, in meters, between two LatLngs. You can optionally specify a custom radius. The radius defaults to the radius of the Earth.
             /// </summary>
-            qreal computeDistanceBetween(googleMaps::LatLng from, googleMaps::LatLng to);
+            void computeDistanceBetween(const googleMaps::LatLng from, const googleMaps::LatLng to);
 
             /// <summary>
             ///
             /// Return Value:  number
             /// Returns the heading from one LatLng to another LatLng. Headings are expressed in degrees clockwise from North within the range [-180,180).
             /// </summary>
-            qreal computeHeading(googleMaps::LatLng from, googleMaps::LatLng to);
+            void computeHeading(const googleMaps::LatLng from, const googleMaps::LatLng to);
 
             /// <summary>
             ///
             /// Return Value:  number
             /// Returns the length of the given path.
             /// </summary>
-            qreal computeLength(QList<googleMaps::LatLng> path);
+            void computeLength(const QList<googleMaps::LatLng> path);
 
             /// <summary>
             ///
             /// Return Value:  LatLng
             /// Returns the LatLng resulting from moving a distance from an origin in the specified heading (expressed in degrees clockwise from north).
             /// </summary>
-            googleMaps::LatLng computeOffset(googleMaps::LatLng from, qreal distance, qreal heading);
+            void computeOffset(const googleMaps::LatLng from, const qreal distance, const qreal heading);
 
             /// <summary>
             /// Returns the location of origin when provided with a LatLng destination, meters travelled and original heading. Headings are expressed in degrees clockwise from North. This function returns null when no solution is available.
             /// </summary>
-            googleMaps::LatLng computeOffsetOrigin(googleMaps::LatLng to, qreal distance, qreal heading);
+            void computeOffsetOrigin(const googleMaps::LatLng to, const qreal distance, const qreal heading);
 
             /// <summary>
             /// Returns the LatLng which lies the given fraction of the way between the origin LatLng and the destination LatLng.
             /// </summary>
-            googleMaps::LatLng interpolate(googleMaps::LatLng from, googleMaps::LatLng to, qreal fraction);            
+            void interpolate(const googleMaps::LatLng from, const googleMaps::LatLng to, const qreal fraction);
             qreal getDistanceResult() const;            
-            googleMaps::LatLng getPositionResult() const;
+            googleMaps::LatLng getPositionResults() const;
 
          signals:
-            void positionResultReceived(const googleMaps::LatLng position);
-            void distanceResultReceived(const qreal distance);
+            void positionResultReceived();
+            void distanceResultReceived();
+            void computeOffsetOriginRequest(qreal toLat, qreal toLng, qreal distance, qreal heading);
+            void interpolateRequest(qreal fromLat, qreal fromLng, qreal toLat, qreal toLng, qreal fraction);
+            void computeOffsetRequest(qreal fromLat, qreal fromLng, qreal distance, qreal heading);
+            //void computeLengthRequest(QVariantList path);
+            void computeHeadingRequest(qreal fromLat, qreal fromLng, qreal toLat, qreal toLng);
+            void computeDistanceBetweenRequest(qreal fromLat, qreal fromLng, qreal toLat, qreal toLng);
+            //void computeAreaRequest(QVariantList path);
 
          public slots:
             void setPositionResult(const googleMaps::LatLng aPositionResult);
@@ -85,4 +94,5 @@ namespace googleMaps
 	};
 }
 
+Q_DECLARE_METATYPE(googleMaps::SphericalGeometry)
 #endif
