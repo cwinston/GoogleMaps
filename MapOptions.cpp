f47f067afe1b660e37c3c8f8b4229a922bbd6aa6
cwinston <cwinston@technotainment.com>
2017-03-08 02:33:02 -0600

    #1640 time:2h state:Done comment:"modified map options to have a serialize function. Also added defines for property names"

--- MapOptions.cpp
+++ MapOptions.cpp
@@ -65,7 +65,8 @@ googleMaps::MapOptions& googleMaps::MapOptions::operator=(const googleMaps::MapO
     return *this;
 }
 
-void googleMaps::MapOptions::updateBackgroundColor(QString backgroundColor) {
+void googleMaps::MapOptions::updateBackgroundColor(const QString backgroundColor)
+{
     this->m_backgroundColor = backgroundColor;
 }
 
@@ -74,7 +75,8 @@ QString googleMaps::MapOptions::getBackgroundColor() const
     return this->m_backgroundColor;
 }
 
-void googleMaps::MapOptions::updateCenter(googleMaps::LatLng center) {
+void googleMaps::MapOptions::updateCenter(const LatLng center)
+{
     this->m_center = center;
 }
 
@@ -93,7 +95,7 @@ bool googleMaps::MapOptions::isDisableDefaultUI() const
     return this->m_disableDefaultUI;
 }
 
-void googleMaps::MapOptions::updateDisableDoubleClickZoom(bool disableDoubleClickZoom)
+void googleMaps::MapOptions::updateDisableDoubleClickZoom(const bool disableDoubleClickZoom)
 {
     this->m_disableDoubleClickZoom = disableDoubleClickZoom;
 }
@@ -102,7 +104,7 @@ bool googleMaps::MapOptions::isDisableDoubleClickZoom() const{
     return this->m_disableDoubleClickZoom;
 }
 
-void googleMaps::MapOptions::updateDraggable(bool draggable)
+void googleMaps::MapOptions::updateDraggable(const bool draggable)
 {
     this->m_draggable = draggable;
 }
@@ -112,7 +114,7 @@ bool googleMaps::MapOptions::isDraggable() const
     return this->m_draggable;
 }
 
-void googleMaps::MapOptions::updateMapTypeControl(bool mapTypeControl)
+void googleMaps::MapOptions::updateMapTypeControl(const bool mapTypeControl)
 {
     this->m_mapTypeControl = mapTypeControl;
 }
@@ -122,7 +124,7 @@ bool googleMaps::MapOptions::hasMapTypeControl() const
     return this->m_mapTypeControl;
 }
 
-void googleMaps::MapOptions::updateMapTypeId(googleMaps::EMapTypeID mapTypeId)
+void googleMaps::MapOptions::updateMapTypeId(const EMapTypeID mapTypeId)
 {
     this->m_mapTypeId = mapTypeId;
 }
@@ -132,7 +134,8 @@ googleMaps::EMapTypeID googleMaps::MapOptions::getMapTypeId() const
     return this->m_mapTypeId;
 }
 
-void googleMaps::MapOptions::updateMaxZoom(int maxZoom) {
+void googleMaps::MapOptions::updateMaxZoom(const int maxZoom)
+{
     this->m_maxZoom = maxZoom;
 }
 
@@ -140,7 +143,8 @@ int googleMaps::MapOptions::getMaxZoom() const {
     return this->m_maxZoom;
 }
 
-void googleMaps::MapOptions::updateMinZoom(int minZoom) {
+void googleMaps::MapOptions::updateMinZoom(const int minZoom)
+{
     this->m_minZoom = minZoom;
 }
 
@@ -149,7 +153,7 @@ int googleMaps::MapOptions::getMinZoom() const
     return this->m_minZoom;
 }
 
-void googleMaps::MapOptions::updateZoom(int zoom)
+void googleMaps::MapOptions::updateZoom(const int zoom)
 {
     this->m_zoom = zoom;
 }
@@ -159,7 +163,7 @@ int googleMaps::MapOptions::getZoom() const
     return this->m_zoom;
 }
 
-void googleMaps::MapOptions::updateZoomControl(bool zoomControl)
+void googleMaps::MapOptions::updateZoomControl(const bool zoomControl)
 {
     this->m_zoomControl = zoomControl;
 }
@@ -169,7 +173,7 @@ bool googleMaps::MapOptions::hasZoomControl() const
     return this->m_zoomControl;
 }
 
-void googleMaps::MapOptions::updateClickableIcons(bool clickableIcons)
+void googleMaps::MapOptions::updateClickableIcons(const bool clickableIcons)
 {
     this->m_clickableIcons = clickableIcons;
 }
@@ -179,7 +183,7 @@ bool googleMaps::MapOptions::hasClickableIcons() const
     return this->m_clickableIcons;
 }
 
-void googleMaps::MapOptions::updateFullscreenControl(bool fullscreenControl)
+void googleMaps::MapOptions::updateFullscreenControl(const bool fullscreenControl)
 {
     this->m_fullscreenControl = fullscreenControl;
 }
@@ -189,7 +193,7 @@ bool googleMaps::MapOptions::hasFullscreenControl() const
     return this->m_fullscreenControl;
 }
 
-void googleMaps::MapOptions::updateHeading(qreal heading)
+void googleMaps::MapOptions::updateHeading(const qreal heading)
 {
     this->m_heading = heading;
 }
@@ -209,7 +213,7 @@ bool googleMaps::MapOptions::hasRotateControl() const
     return this->m_rotateControl;
 }
 
-void googleMaps::MapOptions::updateRotateControlPosition(int rotateControlPosition)
+void googleMaps::MapOptions::updateRotateControlPosition(const int rotateControlPosition)
 {
     this->m_rotateControlPosition = rotateControlPosition;
 }
@@ -219,7 +223,7 @@ int googleMaps::MapOptions::getRotateControlPosition() const
     return this->m_rotateControlPosition;
 }
 
-void googleMaps::MapOptions::updateFullscreenControlPosition(int fullscreenControlPosition)
+void googleMaps::MapOptions::updateFullscreenControlPosition(const int fullscreenControlPosition)
 {
     this->m_fullscreenControlPosition = fullscreenControlPosition;
 }
@@ -229,10 +233,36 @@ int googleMaps::MapOptions::getFullscreenControlPosition() const
     return this->m_fullscreenControlPosition;
 }
 
-void googleMaps::MapOptions::updateZoomControlPosition(int zoomControlPosition) {
+void googleMaps::MapOptions::updateZoomControlPosition(const int zoomControlPosition)
+{
     this->m_zoomControlPosition = zoomControlPosition;
 }
 
-int googleMaps::MapOptions::getZoomControlPosition() const {
+int googleMaps::MapOptions::getZoomControlPosition() const
+{
     return this->m_zoomControlPosition;
+}
+
+QVariantMap googleMaps::MapOptions::serialize() const
+{
+    QVariantMap options;
+    options.insert(MAP_BACKGROUND_COLOR, m_backgroundColor);
+    options.insert(MAP_CENTER, m_center.serialize());
+    options.insert(MAP_DISABLED_DEFAULT_UI, m_disableDefaultUI);
+    options.insert(MAP_DISABLED_DOUBLECLICK, m_disableDoubleClickZoom);
+    options.insert(MAP_DRAGGABLE, m_draggable);
+    options.insert(MAP_MAPTYPECONTROL, m_mapTypeControl);
+    options.insert(MAP_MAPTYPEID, m_mapTypeId);
+    options.insert(MAP_MAXZOOM, m_maxZoom);
+    options.insert(MAP_MINZOOM, m_minZoom);
+    options.insert(MAP_ZOOM, m_zoom);
+    options.insert(MAP_ZOOMCONTROL, m_zoomControl);
+    options.insert(MAP_CLICKABLE_ICONS, m_clickableIcons);
+    options.insert(MAP_FULLSCREEN_CONTROL, m_fullscreenControl);
+    options.insert(MAP_HEADING, m_heading);
+    options.insert(MAP_ROTATE_CONTROL, m_rotateControl);
+    options.insert(MAP_ROTATE_CONTROL_POSITION, m_rotateControlPosition);
+    options.insert(MAP_FULLSCREEN_CONTROL_POSITION, m_fullscreenControlPosition);
+    options.insert(MAP_ZOOMCONTROL_POSITION, m_zoomControlPosition);
+    return options;
 }
--- MapOptions.h
+++ MapOptions.h
@@ -6,6 +6,25 @@ using namespace std;
 
 #include "LatLng.h"
 
+#define MAP_BACKGROUND_COLOR "backgroundColor"
+#define MAP_CENTER "center"
+#define MAP_DISABLED_DEFAULT_UI "disableDefaultUI"
+#define MAP_DISABLED_DOUBLECLICK    "disableDoubleClickZoom"
+#define MAP_DRAGGABLE "draggable"
+#define MAP_MAPTYPECONTROL "mapTypeControl"
+#define MAP_MAPTYPEID "mapTypeId"
+#define MAP_MAXZOOM "maxZoom"
+#define MAP_MINZOOM "minZoom"
+#define MAP_ZOOM "zoom"
+#define MAP_ZOOMCONTROL "zoomControl"
+#define MAP_CLICKABLE_ICONS "clickableIcons"
+#define MAP_FULLSCREEN_CONTROL "fullscreenControl"
+#define MAP_HEADING "heading"
+#define MAP_ROTATE_CONTROL "rotateControl"
+#define MAP_ROTATE_CONTROL_POSITION "rotateControlPosition"
+#define MAP_FULLSCREEN_CONTROL_POSITION "fullscreenControlPosition"
+#define MAP_ZOOMCONTROL_POSITION "zoomControlPosition"
+
 namespace googleMaps
 {
 	class LatLng;
@@ -27,6 +46,7 @@ namespace googleMaps
     class MapOptions : public QObject
 	{
         Q_OBJECT
+        Q_ENUMS(EMapTypeID)
         protected:
             QString m_backgroundColor;
             googleMaps::LatLng m_center;
@@ -53,44 +73,45 @@ namespace googleMaps
             MapOptions(const MapOptions& rhs);
             //assignment operator
             MapOptions& operator=(const MapOptions &rhs);
-            QString getBackgroundColor() const;
-            googleMaps::LatLng getCenter() const;
-            bool isDisableDefaultUI() const;
-            bool isDisableDoubleClickZoom() const;
-            bool isDraggable() const;
-            bool hasMapTypeControl() const;
-            googleMaps::EMapTypeID getMapTypeId() const;
-            int getMaxZoom() const;
-            int getMinZoom() const;
-            int getZoom() const;
-            int getFullscreenControlPosition() const;
-            int getZoomControlPosition() const;
-            bool hasZoomControl() const;
-            bool hasClickableIcons() const;
-            bool hasFullscreenControl() const;
-            qreal getHeading() const;
-            bool hasRotateControl() const;
-            int getRotateControlPosition() const;
+            Q_INVOKABLE QString getBackgroundColor() const;
+            Q_INVOKABLE googleMaps::LatLng getCenter() const;
+            Q_INVOKABLE bool isDisableDefaultUI() const;
+            Q_INVOKABLE bool isDisableDoubleClickZoom() const;
+            Q_INVOKABLE bool isDraggable() const;
+            Q_INVOKABLE bool hasMapTypeControl() const;
+            Q_INVOKABLE googleMaps::EMapTypeID getMapTypeId() const;
+            Q_INVOKABLE int getMaxZoom() const;
+            Q_INVOKABLE int getMinZoom() const;
+            Q_INVOKABLE int getZoom() const;
+            Q_INVOKABLE int getFullscreenControlPosition() const;
+            Q_INVOKABLE int getZoomControlPosition() const;
+            Q_INVOKABLE bool hasZoomControl() const;
+            Q_INVOKABLE bool hasClickableIcons() const;
+            Q_INVOKABLE bool hasFullscreenControl() const;
+            Q_INVOKABLE qreal getHeading() const;
+            Q_INVOKABLE bool hasRotateControl() const;
+            Q_INVOKABLE int getRotateControlPosition() const;
+            QVariantMap serialize() const;
 
         public slots:
-            void updateZoomControlPosition(int zoomControlPosition);
-            void updateFullscreenControlPosition(int fullscreenControlPosition);
-            void updateRotateControlPosition(int rotateControlPosition);
-            void updateRotateControl(bool rotateControl);
-            void updateHeading(qreal heading);
-            void updateFullscreenControl(bool fullscreenControl);
-            void updateClickableIcons(bool clickableIcons);
-            void updateZoomControl(bool zoomControl);
-            void updateZoom(int zoom);
-            void updateMinZoom(int minZoom);
-            void updateMaxZoom(int maxZoom);
-            void updateMapTypeId(googleMaps::EMapTypeID mapTypeId);
-            void updateMapTypeControl(bool mapTypeControl);
-            void updateDraggable(bool draggable);
-            void updateDisableDoubleClickZoom(bool disableDoubleClickZoom);
-            void updateDisableDefaultUI(bool disableDefaultUI);
-            void updateCenter(googleMaps::LatLng center);
-            void updateBackgroundColor(QString backgroundColor);
+            void updateZoomControlPosition(const int zoomControlPosition);
+            void updateFullscreenControlPosition(const int fullscreenControlPosition);
+            void updateRotateControlPosition(const int rotateControlPosition);
+            void updateRotateControl(const bool rotateControl);
+            void updateHeading(const qreal heading);
+            void updateFullscreenControl(const bool fullscreenControl);
+            void updateClickableIcons(const bool clickableIcons);
+            void updateZoomControl(const bool zoomControl);
+            void updateZoom(const int zoom);
+            void updateMinZoom(const int minZoom);
+            void updateMaxZoom(const int maxZoom);
+            void updateMapTypeId(const googleMaps::EMapTypeID mapTypeId);
+            void updateMapTypeControl(const bool mapTypeControl);
+            void updateDraggable(const bool draggable);
+            void updateDisableDoubleClickZoom(const bool disableDoubleClickZoom);
+            void updateDisableDefaultUI(const bool disableDefaultUI);
+            void updateCenter(const googleMaps::LatLng center);
+            void updateBackgroundColor(const QString backgroundColor);
 
 
 	};
